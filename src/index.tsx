34b20adca60b1642bacaa6ca37338cc203bb1637
WDavid Calsin <wdavidcalsin968@gmail.con>
2021-03-02 23:11:07 -0500

    feat: 🎸 redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
 import 'bootstrap-container';
+import App from './App';
+
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import { store } from './store/index';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
 
